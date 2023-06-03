6f06dfa5c1fb165aa951760f3e2f39f503ac868e
soerenab <soren.a.becker@gmail.com>
2023-06-03 20:33:12 +0200

    Debugged and extended ffx_wrapper.py

--- symbolicregression/baselines/ffx_wrapper.py
+++ symbolicregression/baselines/ffx_wrapper.py
@@ -1,28 +1,37 @@
-from typing import List, Union
+from typing import Dict, List, Union
 from ffx import FFXRegressor
-from symbolicregression.model.mixins import (BatchMixin, 
-                                             PredictionIntegrationMixin, 
-                                             FiniteDifferenceMixin)
+from symbolicregression.model.mixins import (
+    BatchMixin, PredictionIntegrationMixin, FiniteDifferenceMixin, MultiDimMixin
+)
 
+import re
 import numpy as np
 
 __all__ = ("FFXWrapper",)
 
-class FFXWrapper(FFXRegressor, BatchMixin, PredictionIntegrationMixin, FiniteDifferenceMixin):
-    
-    # TODO: Does not support vector-values functions.
+# TODO: restrict function class, e.g. no "max", log10
+
+class FFXWrapper(FFXRegressor, BatchMixin, FiniteDifferenceMixin, MultiDimMixin, PredictionIntegrationMixin, ):
     
     def fit(
         self, 
         times: Union[List[np.ndarray], np.ndarray], 
-        trajectories: Union[List, np.ndarray], 
+        trajectories: Union[List, np.ndarray],
+        derivatives: Union[None, np.ndarray] = None,
+        *args, 
         **kwargs,
-    ):  
+    ) -> Dict[int, List[str]]:  
         if isinstance(trajectories, List):
-            self.fit_all(times=times, trajectories=trajectories)
-        y = self.approximate_derivative(trajectory=trajectories, t=times)
-        super().fit(X=trajectories, y=y, **kwargs)
-        return self._get_equations()
+            return self.fit_all(times=times, trajectories=trajectories)
+        if derivatives is None:
+            derivatives = self.approximate_derivative(trajectory=trajectories, times=times).squeeze()
+            if len(derivatives.shape) > 1:
+                return self.fit_components(times, trajectories, derivatives, args, kwargs)
+        super().fit(X=trajectories, y=derivatives, **kwargs)
+        return {0: self._clean_equation(self._get_equations())}
     
-    def _get_equations(self):
+    def _get_equations(self) -> List[str]:
         return [m.str2() for m in self.models_[::-1]]
+    
+    def _clean_equation(self, eqs: List[str]):
+        return [re.sub(pattern=r"(X)(\d+)", repl=r"x_\2", string=eq).replace("^", "**") for eq in eqs]
