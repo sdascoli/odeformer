ddd8155a7f2f74c02faec16b6fe0bc76147a5340
soerenab <soren.a.becker@gmail.com>
2023-06-05 09:42:35 +0200

    Added finite_difference_order and smoother_window_length keywords in FFXWrapper

--- symbolicregression/baselines/ffx_wrapper.py
+++ symbolicregression/baselines/ffx_wrapper.py
@@ -9,25 +9,44 @@ import numpy as np
 
 __all__ = ("FFXWrapper",)
 
-# TODO: restrict function class, e.g. no "max", log10?
-
 class FFXWrapper(
-    FFXRegressor, BatchMixin, FiniteDifferenceMixin, MultiDimMixin, PredictionIntegrationMixin, SympyMixin,
+    FFXRegressor, 
+    BatchMixin, 
+    FiniteDifferenceMixin, 
+    MultiDimMixin, 
+    PredictionIntegrationMixin, 
+    SympyMixin,
 ):
     def fit(
         self, 
         times: Union[List[np.ndarray], np.ndarray], 
         trajectories: Union[List, np.ndarray],
         derivatives: Union[None, np.ndarray] = None,
+        finite_difference_order: Union[None, int] = None,
+        smoother_window_length: Union[None, int] = None,
         *args, 
         **kwargs,
-    ) -> Dict[int, List[str]]:  
+    ) -> Dict[int, List[str]]:
         if isinstance(trajectories, List):
-            return self.fit_all(times=times, trajectories=trajectories)
+            return self.fit_all(
+                times=times, 
+                trajectories=trajectories,
+                finite_difference_order=finite_difference_order,
+                smoother_window_length=smoother_window_length,
+                *args, **kwargs,
+            )
         if derivatives is None:
-            derivatives = self.approximate_derivative(trajectory=trajectories, times=times).squeeze()
+            derivatives = self.approximate_derivative(
+                trajectory=trajectories, 
+                times=times,
+                finite_difference_order=finite_difference_order,
+                smoother_window_length=smoother_window_length,
+            ).squeeze()
             if len(derivatives.shape) > 1:
-                return self.fit_components(times, trajectories, derivatives, args, kwargs)
+                return self.fit_components(
+                    times, trajectories, derivatives, args, kwargs
+                )
+        # TODO: How to restrict function class, e.g. no "max", log10?
         super().fit(X=trajectories, y=derivatives, **kwargs)
         return {0: self._clean_equation(self._get_equations())}
     
