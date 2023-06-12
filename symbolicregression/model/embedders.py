8b4afac91567ccdc998726e2fd1a6685eca30554
soerenab <soren.a.becker@gmail.com>
2023-06-12 09:56:34 +0200

    implemented support for multi-dim trajectories

--- symbolicregression/model/embedders.py
+++ symbolicregression/model/embedders.py
@@ -8,6 +8,7 @@ from typing import List, Optional, Tuple, Union
 from abc import ABC, abstractmethod
 import torch
 import torch.nn as nn
+import numpy as np
 from symbolicregression.utils import to_cuda
 import torch.nn.functional as F
 
@@ -142,7 +143,9 @@ class LinearPointEmbedder(Embedder):
     
 
 class TwoHotEmbedder():
-    
+    # TODO: support masked-language-modeling
+    # TODO: check if compatible with old two-hot-encoding
+    # TODO: add scaling function
     def __init__(
         self, 
         num_embeddings: Union[None, int], # if None, you must supply min_value and max_value
@@ -150,7 +153,6 @@ class TwoHotEmbedder():
         min_value: Union[None, int] = None, # min is included, ignored if num_embeddings is not None
         max_value: Union[None, int] = None, # min is excluded, ignored if num_embeddings is not None
         init_from_arange: bool = False, # useful for debugging
-        require_grad: bool = False, # only applies when init_from_arange
     ):
         """
         Arguments:
@@ -161,9 +163,12 @@ class TwoHotEmbedder():
         - max_value: supremum, this is the first value outside the represented range of values.
         -init_from_arange: If True, initialize embeddings such that an input of 0 gets an embedding vector of zeros, 
             an input of 1 gets an embedding vector of ones, etc.
-        - require_grad: If True, embeddings are learnable, otherwise they will be fixed to their init values.
         
         """
+        self.max_dimension = 1 #TODO
+        self.float_scalar_descriptor_len = 1
+        self.total_dimension = 1 + self.max_dimension
+        self.float_vector_descriptor_len = self.float_scalar_descriptor_len * self.total_dimension
         self.embedding_dim = embedding_dim
         self._min_value = min_value
         self._max_value = max_value
@@ -183,7 +188,6 @@ class TwoHotEmbedder():
                 ).reshape(-1, 1).repeat(1, self.embedding_dim),
                 padding_idx=self.num_embeddings-1,
                 mode="sum",
-                freeze=not require_grad,
             )
         else:
             self.embd_bag = torch.nn.EmbeddingBag(
@@ -193,45 +197,73 @@ class TwoHotEmbedder():
                 mode="sum",
             )
     
-    def __call__(self, inputs: Union[List[torch.Tensor], torch.Tensor]) -> Tuple[torch.Tensor, torch.LongTensor]:
+    def __call__(self, inputs: List[Sequence]) -> Tuple[torch.Tensor, torch.LongTensor]:
         return self.forward(inputs)
     
-    def forward(self, inputs: Union[List[torch.Tensor], torch.Tensor]) -> Tuple[torch.Tensor, torch.LongTensor]:
+    def forward(self, inputs: List[Sequence]) -> Tuple[torch.Tensor, torch.LongTensor]:
+        inputs = self.encode(inputs)
         if isinstance(inputs, List):
-            inputs, seq_lens = self.batch(inputs)
+            inputs, sequences_len = self.batch(inputs)
         else:
-            seq_lens = torch.LongTensor([inputs.shape[0]] * (1 if len(inputs.shape) == 1 else inputs.shape[1]))
-        encoded = self.encode(inputs)
-        return encoded, seq_lens
+            sequences_len = torch.LongTensor(
+                [inputs.shape[0]] * (1 if len(inputs.shape) == 1 else inputs.shape[1])
+            )
+        return self.embed(inputs), sequences_len
+        
     
-    def encode(self, inputs: torch.Tensor) -> torch.Tensor:
-        # inputs has shape = (seq_len, batch size)
-        shape_output = list(inputs.shape)
-        shape_output.append(self.embedding_dim)
+    def encode(self, sequences = List[Sequence]) -> List[torch.Tensor]:
+        res = []
+        for seq in sequences:
+            seq_toks = []
+            for x_toks, y_toks in seq:
+                if isinstance(x_toks, (float, int)):
+                    x_toks = [x_toks]
+                system_dim = len(y_toks)
+                x_toks = [*x_toks,]
+                y_toks = [
+                    *y_toks,
+                    *[
+                        self.embd_bag.padding_idx
+                        for _ in range(
+                            (self.max_dimension - system_dim)
+                            * self.float_scalar_descriptor_len
+                        )
+                    ],
+                ]
+                seq_toks.append([*x_toks, *y_toks])
+            res.append(torch.DoubleTensor(seq_toks))
+        return res
+    
+    def embed(self, inputs: torch.Tensor) -> torch.Tensor:
+        seq_len, batch_size, system_dims = inputs.shape
         # for the two-hot representation we need to specify:
         # - the neighboring bins that support the distribution (=support_idcs)
         # - the probability mass that is assigned to each of the bins (=support_weights)
-        support_idcs = torch.stack((inputs.reshape(-1), inputs.reshape(-1))).T # shape=(seq_len * batch size, 2)
-        # the right bins contains the decimal value, the left bin contains 1-decimal value
+        support_idcs = torch.stack((inputs.reshape(-1), inputs.reshape(-1))).T
+        # right bins contains the decimal value, e.g. 0.6 for input 1.6
         support_weights = support_idcs % 1
+        # left bin contains 1 minus decimal value, e.g. 0.4 for input 1.6
         support_weights[:, 0] = 1 - support_weights[:, 1]
-        support_weights = support_weights.to(torch.float32) # TODO: which dtype to use? needs to be consistent with self.weight
+        support_weights = support_weights.to(torch.float32)
         support_idcs[:, 0] = torch.floor(support_idcs[:, 0])
         support_idcs[:, 1] = torch.ceil(support_idcs[:, 1])
         support_idcs = support_idcs.to(torch.int64)
         support_idcs[support_idcs != self.embd_bag.padding_idx] -= self._min_value
-        out = self.embd_bag(
+        embedding = self.embd_bag(
             input=support_idcs, per_sample_weights=support_weights,
         )
         if self.init_from_arange:
-            out[support_idcs[:,0]==self.embd_bag.padding_idx] = self.pad_embedding_value
-        return out.reshape(shape_output)
+            embedding[support_idcs[:,0]==self.embd_bag.padding_idx] = self.pad_embedding_value
+        # concat embeddings per dimension
+        return embedding.reshape(seq_len, batch_size, -1)
         
     def batch(self, seqs: List[torch.Tensor]) -> Tuple[torch.Tensor, torch.LongTensor]:
         pad_id = self.embd_bag.padding_idx
         lengths = [len(x) for x in seqs]
         bs, slen = len(lengths), max(lengths)
-        sent = torch.Tensor(slen, bs).to(dtype=seqs[0].dtype).fill_(pad_id)
+        sent = torch.DoubleTensor(slen, bs, self.float_vector_descriptor_len).fill_(pad_id)
         for i, seq in enumerate(seqs):
-            sent[0 : len(seq), i] = seq
+            sent[0 : len(seq), i, :] = seq
         return sent, torch.LongTensor(lengths)
+    
+    
