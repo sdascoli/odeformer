769d760704a482ba3726f548827c2467c89b2a60
soerenab <soren.a.becker@gmail.com>
2023-07-02 15:59:45 +0200

    added noise and subsampling

--- run_baselines.sh
+++ run_baselines.sh
@@ -7,19 +7,30 @@ conda activate symbolicregression39
 MODELS=(
     "sindy"
     "sindy_save"
-    "sindy_poly"
     "sindy_poly3"
-    "afp"
-    "feafp"
+    "sindy_poly6"
+    "sindy_poly10"
+    "sindy_full"
+    # "afp"
+    # "feafp"
     "ffx"
-    "eplex"
-    "ehc"
-    "proged"
-    "pysr"
+    # "eplex"
+    # "ehc"
+    # "proged"
+    # "pysr"
 )
 
-for model in "${MODELS[@]}";
+for subsample_ratio in "0" "0.25" "0.5";
 do
-    echo "Evaluting ${model}"
-    python test_baselines_hyperparam_search.py --baseline_model="${model}"
+    for eval_noise_gamma in "0" "0.001" "0.01";
+    do
+        for model in "${MODELS[@]}";
+        do
+            echo "Evaluting ${model} with subsample_ratio=${subsample_ratio} and eval_noise_gamma=${eval_noise_gamma}"
+            python run_baselines.py \
+                --baseline_model="${model}" \
+                --subsample_ratio="${subsample_ratio}" \
+                --eval_noise_gamma="${eval_noise_gamma}"
+        done
+    done
 done
